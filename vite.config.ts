54a4a18198ef24c2176aa347d3cb73cb6c32d237
443a <alirezaab01xx@gmail.com>
2026-06-28 00:52:23 +0330

    fix(build): use relative asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: './',
+
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
